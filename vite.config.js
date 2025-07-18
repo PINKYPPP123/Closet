5aa75fab663321f5afd025de6b98af6669fb4f9a
PP <apple@appledeMacBook-Air.local>
2025-07-18 10:45:13 +0800

    Configure base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/Closet/",
   build: {
     sourcemap: false, // 关闭 source map，防止代码泄漏
   },
